dd584fdb53f8348a97635c8f26c54630e83d017b
Alexandr <ovechko.056@gmail.com>
2018-08-24 22:15:09 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+'''Cloud ML Engine package configuration.'''
+from setuptools import setup, find_packages
+
+setup(name='pix_pix',
+      version='1.0',
+      packages=find_packages(),
+      include_package_data=True,
+      description='.......',
+      author='Alex',
+      author_email='ovechko.056@gmail.com',
+      license='Unlicense',
+      install_requires=[
+          'tensorflow',
+          'numpy',
+          'argparse'],
+      zip_safe=False)
