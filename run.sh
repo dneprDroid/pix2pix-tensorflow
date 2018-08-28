442a200e06a434eaaf8ec7fae58f8b068c8ae044
Alexandr <ovechko.056@gmail.com>
2018-08-28 22:28:56 +0300

    Update python version.

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ export REGION=us-east1
 #gcloud ml-engine local train --job-dir $JOB_DIR --module-name trainer.pix2pix  --package-path ./trainer
 
 
-gcloud ml-engine jobs submit training $JOB_NAME --job-dir $JOB_DIR --python-version 3.7 --runtime-version 1.0 --module-name trainer.pix2pix --package-path ./trainer --region $REGION --  --mode train --which_direction BtoA --input_dir gs://$BUCKET_NAME/facades/facades/train --output_dir gs://$BUCKET_NAME/facades/facades_train
+gcloud ml-engine jobs submit training $JOB_NAME --job-dir $JOB_DIR --python-version "3.5" --runtime-version 1.0 --module-name trainer.pix2pix --package-path ./trainer --region $REGION --  --mode train --which_direction BtoA --input_dir gs://$BUCKET_NAME/facades/facades/train --output_dir gs://$BUCKET_NAME/facades/facades_train
